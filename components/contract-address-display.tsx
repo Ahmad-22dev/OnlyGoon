781ab3c136253e77320da77104188f0b4f2950ee
Ahmad-22dev <ahmadakash22@gmail.com>
2025-04-02 21:41:08 +0200

    Update contract-address-display.tsx

--- components/contract-address-display.tsx
+++ components/contract-address-display.tsx
@@ -11,7 +11,7 @@ export function ContractAddressDisplay() {
   const { toast } = useToast()
 
   // This will be replaced with the actual contract address when provided
-  const contractAddress = "Contract address will be provided soon"
+  const contractAddress = "DQiWZoP4F4hKKLocmdD7j5cPo8CSyBfQUeQSRFnVpump"
 
   const handleCopy = () => {
     navigator.clipboard.writeText(contractAddress)
